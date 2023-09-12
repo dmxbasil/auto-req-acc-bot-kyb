e1e5f934f27533d19b77a7744c144db60a24f950
BASIL » <77526924+dmxbasil@users.noreply.github.com>
2023-09-13 01:13:04 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -2,6 +2,6 @@ from aiohttp import web
 
 routes = web.RouteTableDef()
 
-@routes.get("/", allow_head=true)
+@routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response("dmx_chating") 
